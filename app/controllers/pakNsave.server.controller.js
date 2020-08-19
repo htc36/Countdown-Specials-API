9b61cff877204c757c2993061b7469e63ad6c768
Harry Collard <htc36@uclive.ac.nz>
2020-08-19 15:22:38 +1200

    edited offest value2

--- app/controllers/pakNsave.server.controller.js
+++ app/controllers/pakNsave.server.controller.js
@@ -22,7 +22,7 @@ exports.getSingleProduct = async function(req, res) {
     }
     const query = "select * from distinctProducts where distinctProducts.productId NOT IN (SELECT pakNsaveID " +
         "from linkedSupermarkets WHERE linkedSupermarkets.pakNsaveID = productId) " +
-        "AND category1 = '" + cat1 + "' AND category2 = '" + cat2 + "' LIMIT 1 OFFSET = " + offset
+        "AND category1 = '" + cat1 + "' AND category2 = '" + cat2 + "' LIMIT 1 OFFSET " + offset
 
     try {
         const result = await pakNsave.getSingleUnJoinedProduct(query);
