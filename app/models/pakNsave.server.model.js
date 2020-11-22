6d54edce6d1dc66c5acf375ee661ce7a8b2d8897
Harry Collard <htc36@uclive.ac.nz>
2020-11-22 14:31:38 +1300

    Added id of countdown product to paknsave request

--- app/models/pakNsave.server.model.js
+++ app/models/pakNsave.server.model.js
@@ -32,12 +32,12 @@ exports.getProductsHistory = async function(code, store) {
 
 exports.getConnectedProductHistoryPaknSave = async function(location, code) {
     const q = "SELECT psProducts.quantityType, psProducts.name, psPrices.price, psPrices.date, cdProducts.name, cdProducts.brand, cdProducts.volSize," +
-        " cdPrices.salePrice, cdPrices.date as date2 FROM psProducts JOIN psPrices ON psProducts.productId = psPrices.productId AND psProducts.productId" +
+        " cdPrices.salePrice, cdPrices.date as date2, cdProducts.code FROM psProducts JOIN psPrices ON psProducts.productId = psPrices.productId AND psProducts.productId" +
         " = '" + code + "' JOIN linkedSupermarkets on psPrices.productId = linkedSupermarkets. pakNsaveID AND psPrices.store = '" + location + "' LEFT JOIN " +
         "cdPrices ON linkedSupermarkets.countdownID = cdPrices.code AND cdPrices.date = psPrices.date LEFT JOIN cdProducts ON cdPrices.code = cdProducts.code " +
         "UNION " +
         "SELECT psProducts.quantityType, psProducts.name, psPrices.price, psPrices.date, cdProducts.name, cdProducts.brand, cdProducts.volSize, " +
-        "cdPrices.salePrice, cdPrices.date FROM cdProducts JOIN cdPrices ON cdPrices.code = cdProducts.code JOIN linkedSupermarkets on cdProducts.code = " +
+        "cdPrices.salePrice, cdPrices.date, cdProducts.code FROM cdProducts JOIN cdPrices ON cdPrices.code = cdProducts.code JOIN linkedSupermarkets on cdProducts.code = " +
         "linkedSupermarkets.countdownID JOIN psProducts ON linkedSupermarkets.pakNsaveID = psProducts.productId AND psProducts.productId = '" + code + "'" +
         " LEFT JOIN psPrices ON psProducts.productId = psPrices.productId AND cdPrices.date = psPrices.date AND psPrices.store = '" + location + "' " +
         "ORDER BY COALESCE(date, date2)"
